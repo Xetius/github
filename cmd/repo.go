afaf631536e25dfde2d67632a9a16e159fd98e98
Chris Hudson <chris@xetius.com>
2018-09-20 19:35:01 +0100

    Added repo root command

---
+++ cmd/repo.go
@@ -0,0 +1,29 @@
+// Copyright © 2018 Chris Hudson <chris@xetius.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var repoCmd = &cobra.Command{
+	Use:   "repo",
+	Short: "Perform a function on a repository",
+	Long: ``,
+}
+
+func init() {
+	rootCmd.AddCommand(repoCmd)
+}
