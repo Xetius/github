c4a6f943e5a5b1645d45a6fc9d48fd998b6b7c31
Chris Hudson <chris@xetius.com>
2018-09-20 20:53:57 +0100

    Tests

---
+++ main_test.go
@@ -0,0 +1,106 @@
+// Copyright © 2018 Chris Hudson <chris@xetius.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"testing"
+)
+
+var update = flag.Bool("update", false, "update golden files")
+
+var binaryName = "build/0.0.1/darwin_amd64/github"
+
+func fixturePath(t *testing.T, fixture string) string {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatalf("problems recovering caller information")
+	}
+
+	return filepath.Join(filepath.Dir(filename), fixture)
+}
+
+func writeFixture(t *testing.T, fixture string, content []byte) {
+	err := ioutil.WriteFile(fixturePath(t, fixture), content, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func loadFixture(t *testing.T, fixture string) string {
+	content, err := ioutil.ReadFile(fixturePath(t, fixture))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return string(content)
+}
+
+func TestMain(m *testing.M) {
+	makeCmd := exec.Command("make", "build")
+	err := makeCmd.Run()
+	if err != nil {
+		fmt.Printf("could not make binary for %s: %v", binaryName, err)
+		os.Exit(1)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestCliArgs(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    []string
+		fixture string
+	}{
+		{"no arguments", []string{}, "fixtures/no-args.golden"},
+		{"one argument", []string{"repo"}, "fixtures/repo-arg.golden"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, err := os.Getwd()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			cmd := exec.Command(path.Join(dir, binaryName), tt.args...)
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if *update {
+				writeFixture(t, tt.fixture, output)
+			}
+
+			actual := string(output)
+
+			expected := loadFixture(t, tt.fixture)
+
+			if !reflect.DeepEqual(actual, expected) {
+				t.Fatalf("actual = %s, expected = %s", actual, expected)
+			}
+		})
+	}
+}
